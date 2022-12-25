375828838286e45f05752a58b6e29bb45d88296e
suap-dev <suap.dev@gmail.com>
2022-12-25 15:15:17 +0100

    star moving

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, transform};
 
 fn main() {
     App::new()    
         .add_startup_system(setup_camera)
         .add_startup_system(spawn_snake)
+        .add_system(snake_movement)
         .add_plugins(DefaultPlugins)
         .run();
 }
@@ -17,9 +18,10 @@ struct SnakeHead;
 
 const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
 
+// spawning new entity build from..
 fn spawn_snake(mut commands: Commands) {
     commands
-        // quite default SpriteBundle
+        // ..quite default SpriteBundle, and..
         .spawn_bundle(SpriteBundle {            
             // default Sprite with only changed color
             sprite: Sprite {
@@ -33,5 +35,12 @@ fn spawn_snake(mut commands: Commands) {
             },
             ..default()
         })
+        // .. SnakeHead Component
         .insert(SnakeHead);
+}
+
+fn snake_movement(mut variable: Query<(&SnakeHead, &mut Transform)>) {
+    for (_head, mut transform) in variable.iter_mut() {
+        transform.translation.y += 2.0;
+    }
 }
