315df1b33efb16ee11ccffdf00031b0f90d875a3
suap-dev <suap.dev@gmail.com>
2022-12-25 15:08:00 +0100

    snake head

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use bevy::prelude::*;
 
-const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
-
 fn main() {
     App::new()    
         .add_startup_system(setup_camera)
+        .add_startup_system(spawn_snake)
         .add_plugins(DefaultPlugins)
         .run();
 }
@@ -15,3 +14,24 @@ fn setup_camera(mut commands: Commands) {
 
 #[derive(Component)]
 struct SnakeHead;
+
+const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+
+fn spawn_snake(mut commands: Commands) {
+    commands
+        // quite default SpriteBundle
+        .spawn_bundle(SpriteBundle {            
+            // default Sprite with only changed color
+            sprite: Sprite {
+                color: SNAKE_HEAD_COLOR,
+                ..default()
+            },
+            // and default Transform with just adjusted scale
+            transform: Transform {
+                scale: Vec3::new(10.0, 10.0, 10.0),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(SnakeHead);
+}
