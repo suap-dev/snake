83bc3328c9a07f5b99a8ee965559a50e8405139f
suap-dev <suap.dev@gmail.com>
2022-12-25 15:03:34 +0100

    add camera

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use bevy::prelude::*;
 
+const SNAKE_HEAD_COLOR: Color = Color::rgb(0.7, 0.7, 0.7);
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()    
+        .add_startup_system(setup_camera)
+        .add_plugins(DefaultPlugins)
+        .run();
 }
 
 fn setup_camera(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
+
+#[derive(Component)]
+struct SnakeHead;
