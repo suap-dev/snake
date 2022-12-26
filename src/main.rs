bde6086c7fe24ecd5cf55bed4875ff81619cf855
suap-dev <suap.dev@gmail.com>
2022-12-26 03:44:34 +0100

    add size_scaling System
    not yet used

--- src/main.rs
+++ src/main.rs
@@ -85,3 +85,14 @@ fn snake_movement(
         }
     }
 }
+
+fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Transform)>) {
+    let window = windows.get_primary().unwrap();
+    for (sprite_size, mut transform) in q.iter_mut() {
+        transform.scale = Vec3::new(
+            (sprite_size.width / ARENA_WIDTH as f32) * (window.width() as f32),
+            (sprite_size.height / ARENA_HEIGHT as f32) * (window.height() as f32),
+            1.0,
+        );
+    }
+}
