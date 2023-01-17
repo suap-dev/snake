0e22f505db504a1209a05288f2c6cff9112b4df6
suap-dev <suap.dev@gmail.com>
2023-01-17 22:43:44 +0100

    clarify size_scalling and position_translation

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -53,43 +53,45 @@ pub fn snake_movement(
 
 // TODO: make this readable
 pub fn size_scaling(windows: Res<Windows>, mut q: Query<(&components::Size, &mut Transform)>) {
-    // The sizing logic goes like so:
-    // if something has a width of 1 in a grid of 40,
-    // and the window is 400px across,
-    // then it should have a width of 10.
-    // -- https://mbuffett.com/posts/bevy-snake-tutorial/
     let window = windows.get_primary().unwrap();
+
+    let width_ratio = window.width() / ARENA_WIDTH as f32;
+    let height_ratio = window.height() / ARENA_HEIGHT as f32;
+
     for (sprite_size, mut transform) in q.iter_mut() {
-        transform.scale = Vec3::new(
-            sprite_size.width * (window.width() / ARENA_WIDTH as f32),
-            sprite_size.height * (window.height() / ARENA_HEIGHT as f32),
-            1.0,
-        );
+        let scaled_width = sprite_size.width * width_ratio;
+        let scaled_height = sprite_size.height * height_ratio;
+
+        transform.scale = Vec3::new(scaled_width, scaled_height, 1.0);
     }
 }
 
-// TODO: make this readable
 pub fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
-    // The position translation:
-    // if an item’s x coordinate is at 5 in our system,
-    // the width in our system is 10, and the window width is 200,
-    // then the coordinate should be 5 / 10 * 200 - 200 / 2.
-    // We subtract half the window width because our coordinate system
-    // starts at the bottom left, and Translation starts from the center.
-    // We then add half the size of a single tile, because we want our sprites
-    // bottom left corner to be at the bottom left of a tile not the center.
-    // -- https://mbuffett.com/posts/bevy-snake-tutorial/
-    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
-        let tile_size = bound_window / bound_game;
-        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
-    }
     let window = windows.get_primary().unwrap();
+
+    let window_width = window.width() as f32;
+    let window_height = window.height() as f32;
+
+    let game_width = ARENA_WIDTH as f32;
+    let game_height = ARENA_HEIGHT as f32;
+
+    let tile_size_x = window_width / game_width;
+    let tile_size_y = window_height / game_height;
+
+    let half_window_width = window_width / 2.;
+    let half_window_height = window_height / 2.;
+
+    let half_tile_size_x = tile_size_x / 2.;
+    let half_tile_size_y = tile_size_y / 2.;
+
     for (pos, mut transform) in q.iter_mut() {
-        transform.translation = Vec3::new(
-            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
-            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
-            0.0,
-        );
+        let x_ratio = pos.x as f32 / game_width;
+        let y_ratio = pos.y as f32 / game_height;
+
+        let x = (x_ratio * window_width) + half_tile_size_x - half_window_width;
+        let y = (y_ratio * window_height) + half_tile_size_y - half_window_height;
+
+        transform.translation = Vec3::new(x, y, 0.0);
     }
 }
 
