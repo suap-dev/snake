4dcb6a2b901890faefb505e099326a52c3df31eb
suap-dev <suap.dev@gmail.com>
2023-01-17 12:18:16 +0100

    add size_scaling and position_scaling

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -52,17 +52,41 @@ pub fn snake_movement(
 }
 
 pub fn size_scaling(windows: Res<Windows>, mut q: Query<(&components::Size, &mut Transform)>) {
+    // The sizing logic goes like so:
+    // if something has a width of 1 in a grid of 40,
+    // and the window is 400px across,
+    // then it should have a width of 10.
+    // -- https://mbuffett.com/posts/bevy-snake-tutorial/
     let window = windows.get_primary().unwrap();
     for (sprite_size, mut transform) in q.iter_mut() {
-        // The sizing logic goes like so:
-        // if something has a width of 1 in a grid of 40,
-        // and the window is 400px across,
-        // then it should have a width of 10.
-        // -- https://mbuffett.com/posts/bevy-snake-tutorial/
         transform.scale = Vec3::new(
             sprite_size.width * (window.width() / ARENA_WIDTH as f32),
             sprite_size.height * (window.height() / ARENA_HEIGHT as f32),
             1.0,
+        );
+    }
+}
+
+pub fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
+    // The position translation:
+    // if an item’s x coordinate is at 5 in our system,
+    // the width in our system is 10, and the window width is 200,
+    // then the coordinate should be 5 / 10 * 200 - 200 / 2.
+    // We subtract half the window width because our coordinate system
+    // starts at the bottom left, and Translation starts from the center.
+    // We then add half the size of a single tile, because we want our sprites
+    // bottom left corner to be at the bottom left of a tile not the center.
+    // -- https://mbuffett.com/posts/bevy-snake-tutorial/
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+    }
+    let window = windows.get_primary().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0,
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,13 @@ fn main() {
 
     game.add_system(snake_movement);
 
+    game.add_system_set_to_stage(
+        CoreStage::PostUpdate,
+        SystemSet::new()
+            .with_system(position_translation)
+            .with_system(size_scaling),
+    );
+
     game.run();
 }
+
