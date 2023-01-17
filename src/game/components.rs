dbcb82c254231e78611e030e9cf4ecfbfab7a536
suap-dev <suap.dev@gmail.com>
2023-01-18 00:35:31 +0100

    update components

--- src/game/components.rs
+++ src/game/components.rs
@@ -1,12 +1,33 @@
 use bevy::prelude::Component;
 #[derive(Component)]
-pub struct SnakeHead;
+pub struct SnakeHead{
+    direction: Direction,
+}
 
-#[derive(Component)]
+#[derive(Component, Copy, Clone, PartialEq, Eq)]
 pub struct Position {
     pub x: i32,
     pub y: i32,
 }
+
+#[derive(PartialEq, Copy, Clone)]
+pub enum Direction {
+    Left,
+    Up,
+    Right,
+    Down,
+}
+impl Direction {
+    fn opposite(self) -> Self {
+        match self {
+            Self::Left => Self::Right,
+            Self::Up => Self::Down,
+            Self::Right => Self::Left,
+            Self::Down => Self::Up,
+        }
+    }
+}
+
 
 #[derive(Component)]
 pub struct Size {
