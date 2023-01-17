4a69300e3fa7655f0fb45406ff60f9a87f6fa937
suap-dev <suap.dev@gmail.com>
2023-01-17 12:25:31 +0100

    add WindowDescriptor -> resize window

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ use game::systems::*;
 fn main() {
     let mut game = App::new();
 
-    game.add_plugins(DefaultPlugins);
+    game.insert_resource(WindowDescriptor{
+        width: 800.0,
+        height: 800.0,
+        title: "Snake".to_string(),
+        ..default()
+    });
+
+    game.add_plugins(DefaultPlugins);    
 
     game.add_startup_system(setup_camera)
         .add_startup_system(spawn_snake);
